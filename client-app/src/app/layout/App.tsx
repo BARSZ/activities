9a82c5cceae840270cbe03352ddf1004f5896d25
Vasil Simeonov <vasko3107@gmail.com>
2022-04-06 17:06:54 +0300

    redesign of dashboard and activity details page

--- client-app/src/app/layout/App.tsx
+++ client-app/src/app/layout/App.tsx
@@ -14,18 +14,18 @@ function App() {
   return (
     <Fragment>
       <Route exact path='/' component={HomePage} />
-      <Route 
-      path={'/(.+)'}
-      render={() => (
-        <Fragment>
-          <NavBar />
-          <Container style={{marginTop: '7em'}}>
-            <Route exact path='/activities' component={ActivityDashboard} />
-            <Route path='/activities/:id' component={ActivityDetails} />
-            <Route key={location.key} path={['/createActivity', '/manage/:id']} component={ActivityForm} />
-      </Container>
-        </Fragment>
-      )}
+      <Route
+        path={'/(.+)'}
+        render={() => (
+          <Fragment>
+            <NavBar />
+            <Container style={{ marginTop: '7em' }}>
+              <Route exact path='/activities' component={ActivityDashboard} />
+              <Route path='/activities/:id' component={ActivityDetails} />
+              <Route key={location.key} path={['/createActivity', '/manage/:id']} component={ActivityForm} />
+            </Container>
+          </Fragment>
+        )}
       />
     </Fragment>
   );
